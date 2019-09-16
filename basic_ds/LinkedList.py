17cf87d7c995a8e997e27db1aae7a5b3297b76d2
saviovincent <savio.vincent24@gmail.com>
2019-09-15 17:16:30 -0700

    basic linkedlist implemented

--- basic_ds/LinkedList.py
+++ basic_ds/LinkedList.py
@@ -1,3 +1,4 @@
+# LinkedList Implementation
 class Node:
 
     def __init__(self, data):
@@ -11,38 +12,86 @@ class LinkedList:
         self.head = None
         self.current = None
 
-    def add(self, node):
+    def addLast(self, node):
 
         if self.head is None:
             self.head = node
             self.current = node
 
+        # else:
+        #     self.current.next = node
+        #     self.current = self.current.next
+
+        # through single pointer
+
         else:
-            self.current.next = node
-            self.current = self.current.next
+            tmp = self.head
+            while tmp.next is not None:
+                tmp = tmp.next
+            tmp.next = node
 
     def contents(self):
 
         temp = self.head
 
         while temp is not None:
-
             print temp.data
             temp = temp.next
 
-
     def addFirst(self, node):
 
-    def addLast(self, node):
+        if self.head is None:
+            self.head = node
+            self.current = node
 
-    def addMiddle(self, node):
+        else:
+            node.next = self.head
+            self.head = node
 
-    def removeFirst(self, node):
-    def removeLast(self, node):
+    def removeFirst(self):
 
-    def removeMiddle(self, node):
+        if self.head is None:
+            print "Nothing to remove"
+        else:
+            self.head = self.head.next
+
+    def removeLast(self):
+
+        if self.head is None:
+            print "Nothing to remove"
+        elif self.head.next is None:
+            self.head = None
+        else:
+            prev = None
+            current = self.head
+            while current.next is not None:
+                prev = current
+                current = current.next
+            prev.next = None
 
     def peekFirst(self):
+        return self.head.data
 
     def peekLast(self):
+        # return self.current.data
 
+        # if only 1 pointer
+        tmp = self.head
+        while tmp.next is not None:
+            tmp = tmp.next
+        return tmp.data
+
+
+def main():
+    ll = LinkedList()
+
+    ll.addLast(Node(1))
+    ll.addLast(Node(2))
+    ll.addLast(Node(3))
+
+    ll.removeLast()
+    ll.contents()
+
+
+if __name__ == '__main__':
+    main()
